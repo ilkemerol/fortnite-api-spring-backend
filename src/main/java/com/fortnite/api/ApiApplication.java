76b0b957f8cd8072cc868122edf8cf3a1b3f867d
ilkem.erol <ilkem.erol@hotmail.com>
2018-07-18 20:57:50 +0300

    Cron Job TEST Deployment

--- src/main/java/com/fortnite/api/ApiApplication.java
+++ src/main/java/com/fortnite/api/ApiApplication.java
@@ -3,12 +3,14 @@ package com.fortnite.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import jodd.http.HttpRequest;
 import jodd.http.HttpResponse;
 
 @SpringBootApplication
 @EnableCaching
+@EnableScheduling
 public class ApiApplication {
 
 	public static void main(String[] args) {
