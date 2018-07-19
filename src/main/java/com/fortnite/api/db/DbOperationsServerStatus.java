c4597644b0d71862d34f205a88123adc0898f09f
ilkemerol <ilkemerol@172.20.10.2>
2018-07-19 14:38:09 +0300

    Daily Item Shop DB Impl and set cron job

--- src/main/java/com/fortnite/api/db/DbOperationsServerStatus.java
+++ src/main/java/com/fortnite/api/db/DbOperationsServerStatus.java
@@ -4,6 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.fortnite.api.entity.ServerStatus;
 
-public interface DbOperations extends CrudRepository<ServerStatus, Integer>{
+public interface DbOperationsServerStatus extends CrudRepository<ServerStatus, Integer>{
 
 }
---
+++ src/main/java/com/fortnite/api/entity/DailyItemShop.java
@@ -0,0 +1,44 @@
+package com.fortnite.api.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "dailyitemshop")
+public class DailyItemShop {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer id;
+	
+	private String date;
+	
+	private String data;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+}
